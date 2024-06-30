7de84b021db527dc80dafa646d6224b8ce121de6
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-06-30 19:56:59 +0200

    created hide cursor script

---
+++ Assets/Scripts/HideCursor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursor : MonoBehaviour {
+    void Start() {
+        // HIDE CURSOR AT THE START OF THE GAME
+        Cursor.visible = false;
+    }
+}
