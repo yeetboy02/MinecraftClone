b14882f4d8d78e2a15dc4d8612541b57d43c6094
yeetboy02 <127201081+yeetboy02@users.noreply.github.com>
2024-06-30 22:52:06 +0200

    created basic block placing/breaking logic

---
+++ Assets/Scripts/PlayerBlockInteraction.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlockInteraction : MonoBehaviour {
+
+    #region Parameters
+
+    [SerializeField] private GameObject blockPrefab;
+
+    [SerializeField] private LayerMask blockLayerMask;
+
+    private float blockWidth = 1.0f;
+
+    private float maxDistance = 10f;
+
+    private float minPlaceDistance = 1.5f;
+
+    #endregion
+
+    #region Variables
+
+    private GameObject currTargetBlock = null;
+
+    private Vector3 currTargetFaceNormal = Vector3.zero;
+
+    private bool currBlockPlaceable = false;
+
+    #endregion
+
+    void Update() {
+        // GET CURRENT TARGET BLOCK
+        UpdateCurrTarget();
+    }
+
+    #region TargetBlock
+
+    private void UpdateCurrTarget() {
+        RaycastHit hit;
+
+        // GET PLAYER CAMERA
+        Camera playerCamera = Camera.main;
+
+        // CHECK RAYCAST
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, maxDistance, blockLayerMask)) {
+            // SET IF BLOCK CURRENTLY PLACABLE
+            currBlockPlaceable = hit.distance < minPlaceDistance ? false : true;
+
+            // SET CURRENT TARGET BLOCK
+            currTargetBlock = hit.collider.gameObject;
+
+            // SET CURRENT TARGET POSITION
+            currTargetFaceNormal = hit.normal;
+
+            Debug.Log(hit.normal);
+        }
+        else {
+            // RESET CURRENT TARGET BLOCK
+            currTargetBlock = null;
+
+            // RESET CURRENT TARGET POSITION
+            currTargetFaceNormal = Vector3.zero;
+
+            // RESET CURRENT BLOCK PLACABLE
+            currBlockPlaceable = false;
+        }
+    }
+
+    #endregion
+
+    #region BreakBlock
+
+    public void OnBreakBlock() {
+        BreakBlock();
+    }
+
+    private void BreakBlock() {
+        // CHECK IF TARGET BLOCK EXISTS
+        if (currTargetBlock == null) return;
+
+        // DESTROY AND RESET TARGET BLOCK
+        Destroy(currTargetBlock);
+        currTargetBlock = null;
+    }
+
+    #endregion
+
+
+    #region PlaceBlock
+
+    public void OnPlaceBlock() {
+        PlaceBlock();
+    }
+
+    private void PlaceBlock() {
+        // CHECK IF CURRENTLY A BLOCK PLACABLE
+        if (!currBlockPlaceable) return;
+
+        // INSTANTIATE BLOCK AT POSITION BLOCK WIDTH AWAY FROM TARGET BLOCK FACE
+        Instantiate(blockPrefab, currTargetBlock.transform.position + currTargetFaceNormal * blockWidth, Quaternion.identity);
+    }
+
+    #endregion
+
+}
